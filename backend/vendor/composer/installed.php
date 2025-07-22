ace2120508cf57f23ab24ce58f9f6281198d6e77
MaverickYoan <ensemblepourlafetelibre@gmail.com>
2025-07-22 02:18:59 +0200

    symfony

---
+++ backend/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b1f57a1dd0eec36667c4979e9ba4b17a1505f773',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b1f57a1dd0eec36667c4979e9ba4b17a1505f773',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
